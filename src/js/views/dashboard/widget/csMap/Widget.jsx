0741227a2401dbaa84f342a4509847474bd49809
Luis Felipe Furlan Cruz <lfcruz@cpqd.com.br>
2020-12-08 18:25:40 -0300

    Added totalizers

--- src/js/views/dashboard/widget/csMap/Widget.jsx
+++ src/js/views/dashboard/widget/csMap/Widget.jsx
@@ -10,47 +10,75 @@ import 'react-leaflet-markercluster/dist/styles.min.css';
 import 'leaflet/dist/images/marker-shadow.png';
 
 export default ({ id, onDelete, onPin, data, config }) => {
-  const [totalGreen, setTotalGreen] = useState(0);
-  const [totalYellow, setTotalYellow] = useState(0);
-  const [totalRed, setTotalRed] = useState(0);
-  // let totalGreen = 0;
-  // let totalYellow = 0;
-  // let totalRed = 0;
+  const [markersCounter, setMarkersCounter] = useState({
+    red: 0,
+    yellow: 0,
+    green: 0,
+  });
   const [markers, setMarkers] = useState([]);
   const [bounds, setBounds] = useState([[0, 0]]);
 
   const mapRef = useRef();
   const { clientHeight, clientWidth } = !mapRef.current
-    ? { clientHeight: 0, clientWidth: 0 }
+    ? {
+        clientHeight: 0,
+        clientWidth: 0,
+      }
     : mapRef.current.container;
 
   useEffect(() => {
     mapRef.current.leafletElement.invalidateSize();
   }, [clientHeight, clientWidth]);
 
-  const getMarkerColorByTime = time => {
+  const getDiffTime = time => {
     const now = moment.utc();
     const end = moment(time);
-    const hour = moment.duration(now.diff(end)).asHours();
+    return moment.duration(now.diff(end)).asHours();
+  };
+
+  const getMarkerColorByTime = time => {
+    const hour = getDiffTime(time);
     if (hour <= 1) {
-      console.log('green');
-      // totalGreen += 1;
       return getMarkerColor('#008b02');
     }
     if (hour <= 2) {
-      console.log('yellow');
-      // totalYellow += 1;
       return getMarkerColor('#fccb00');
     }
-    console.log('red');
-    // totalRed += 1;
     return getMarkerColor('#b80000');
   };
 
+  const countMarkers = (time, { red, yellow, green }) => {
+    const hour = getDiffTime(time);
+    if (hour <= 1) {
+      return {
+        red,
+        yellow,
+        green: green + 1,
+      };
+    }
+    if (hour <= 2) {
+      return {
+        red,
+        yellow: yellow + 1,
+        green,
+      };
+    }
+    return {
+      red: red + 1,
+      yellow,
+      green,
+    };
+  };
+
   useEffect(() => {
+    let counter = {
+      red: 0,
+      yellow: 0,
+      green: 0,
+    };
     setMarkers(
       Object.keys(data || {}).map(key => {
-        setTotalRed(totalRed + 1);
+        counter = countMarkers(data[key].timestamp, counter);
         return (
           <Marker
             position={data[key].value}
@@ -63,7 +91,7 @@ export default ({ id, onDelete, onPin, data, config }) => {
         );
       }),
     );
-
+    setMarkersCounter(counter);
     setBounds(
       _.isEmpty(data)
         ? [[0, 0]]
@@ -71,10 +99,54 @@ export default ({ id, onDelete, onPin, data, config }) => {
     );
   }, [data]);
 
-  console.log('-----------------------------');
-  console.log(`totalRed: ${totalRed}`);
-  console.log(`totalGreen: ${totalGreen}`);
-  console.log(`totalYellow: ${totalYellow}`);
+  const Totalizer = () => {
+    const { red, green, yellow } = markersCounter;
+    const itemStyle = (color, position) => {
+      let stylePosition = '0';
+      let styleColor = '#EB5757';
+      if (position === 'bottom') stylePosition = '0 0 6px 6px';
+      if (position === 'top') stylePosition = '6px 6px 0 0';
+
+      if (color === 'red') styleColor = '#F9CDCD';
+      if (color === 'green') styleColor = '#BFE7D0';
+      if (color === 'yellow') styleColor = '#FCEFCA';
+      return {
+        height: 28,
+        width: 48,
+        borderRadius: stylePosition,
+        background: styleColor,
+        textAlign: 'center',
+        color: 'rgb(0 0 0 / 87%);',
+        fontSize: 16,
+        lineHeight: '28px',
+      };
+    };
+    const titileStyle = {
+      margin: '0 0 10px 0',
+      fontSize: 18,
+    };
+    return (
+      <div
+        style={{
+          background: 'white',
+          position: 'absolute',
+          bottom: 30,
+          right: 20,
+          zIndex: 999,
+          borderRadius: 5,
+          border: 'solid 1px #eaeaea',
+          padding: 10,
+          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        }}
+      >
+        <div style={titileStyle}>Total</div>
+        <div style={itemStyle('green', 'top')}>{green}</div>
+        <div style={itemStyle('yellow', 'middle')}>{yellow}</div>
+        <div style={itemStyle('red', 'bottom')}>{red}</div>
+      </div>
+    );
+  };
+
   return (
     <WidgetCard id={id} onDelete={onDelete} onPin={onPin} config={config}>
       <Map
@@ -97,6 +169,7 @@ export default ({ id, onDelete, onPin, data, config }) => {
         />
         {markers}
       </Map>
+      <Totalizer />
     </WidgetCard>
   );
 };
